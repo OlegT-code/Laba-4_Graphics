c53e736eaeea3dcbc4a2fb8ff8bf55b27c14b60f
User <User@WIN-AK2SGH7E72N>
2020-09-22 01:47:51 +0300

    Добавьте файлы проекта.

---
+++ CompGraphics_Laba3/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace CompGraphics_Laba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.InFile = new System.Windows.Forms.Button();
+            this.ShowText = new System.Windows.Forms.Button();
+            this.ClearFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1157, 483);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // InFile
+            // 
+            this.InFile.Location = new System.Drawing.Point(110, 529);
+            this.InFile.Name = "InFile";
+            this.InFile.Size = new System.Drawing.Size(138, 50);
+            this.InFile.TabIndex = 1;
+            this.InFile.Text = "Записать в файл";
+            this.InFile.UseVisualStyleBackColor = true;
+            this.InFile.Click += new System.EventHandler(this.InFile_Click_1);
+            // 
+            // ShowText
+            // 
+            this.ShowText.Location = new System.Drawing.Point(532, 529);
+            this.ShowText.Name = "ShowText";
+            this.ShowText.Size = new System.Drawing.Size(112, 50);
+            this.ShowText.TabIndex = 2;
+            this.ShowText.Text = "Отображение";
+            this.ShowText.UseVisualStyleBackColor = true;
+            this.ShowText.Click += new System.EventHandler(this.ShowText_Click_1);
+            // 
+            // ClearFile
+            // 
+            this.ClearFile.Location = new System.Drawing.Point(968, 529);
+            this.ClearFile.Name = "ClearFile";
+            this.ClearFile.Size = new System.Drawing.Size(100, 50);
+            this.ClearFile.TabIndex = 3;
+            this.ClearFile.Text = "Очистка";
+            this.ClearFile.UseVisualStyleBackColor = true;
+            this.ClearFile.Click += new System.EventHandler(this.ClearFile_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 653);
+            this.Controls.Add(this.ClearFile);
+            this.Controls.Add(this.ShowText);
+            this.Controls.Add(this.InFile);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button InFile;
+        private System.Windows.Forms.Button ShowText;
+        private System.Windows.Forms.Button ClearFile;
+    }
+}
+
---
+++ CompGraphics_Laba3/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CompGraphics_Laba3
+{
+    public partial class Form1 : Form
+    {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Graphics g;
+        string filename = @"Strings.txt";
+        string[] sm = {
+            "First line", "Second line", "Third line",
+            "Fourth line", "Fifth line", "Sixth line",
+            "Seventh line", "Eighth line", "Ninth line",
+            "Tenth line", "Eleventh line" };
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+        }
+
+        private void ClearFile_Click_1(object sender, EventArgs e)
+        {
+            g.Clear(Color.FromArgb(255, 255, 255));
+            File.Delete(filename);
+            MessageBox.Show("Файл Strings.txt удалён!");
+        }
+
+        private void ShowText_Click_1(object sender, EventArgs e)
+        {
+            int k = 0;
+            try
+            {
+                StreamReader f = new StreamReader(new FileStream(filename,
+                FileMode.Open, FileAccess.Read));
+                for (int i = 0; i < 11; i++) { sm[i] = f.ReadLine(); }
+                f.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+            pictureBox1.BackColor = Color.FromName("Azure");
+            pictureBox1.Refresh();
+
+            for (int i = 0; i < 11; i++)
+            {
+                if ((i >= 0) && (i < 8))
+                {
+                    Font fn = new Font("Courier Now", 18, FontStyle.Regular);
+                    StringFormat sf =
+                    (StringFormat)StringFormat.GenericTypographic.Clone();
+                    sf.Alignment = StringAlignment.Center;
+                    sf.LineAlignment = StringAlignment.Far;
+                    g.DrawString(sm[i], fn, Brushes.Blue,
+                    new RectangleF(0, -130 + i * 20, pictureBox1.Size.Width - 20,
+                    pictureBox1.Size.Height - 20), sf);
+                    fn.Dispose();
+                }
+                if ((i >= 8) && (i < 10))
+                {
+                    k = i - 9;
+                    Font fn = new Font("Arial", 24, FontStyle.Italic);
+                    StringFormat sf =
+                    (StringFormat)StringFormat.GenericTypographic.Clone();
+                    sf.FormatFlags = StringFormatFlags.DirectionVertical;
+                    sf.Alignment = StringAlignment.Near;
+                    sf.LineAlignment = StringAlignment.Far;
+                    g.DrawString(sm[i], fn, Brushes.Black,
+                    new RectangleF(0 + k * 26, 10, pictureBox1.Size.Width - 20,
+                    pictureBox1.Size.Height - 20), sf);
+                    fn.Dispose();
+                }
+                if (i == 10)
+                {
+                    Font fn = new Font("Times New Roman", 50, FontStyle.Bold);
+                    StringFormat sf =
+                    (StringFormat)StringFormat.GenericTypographic.Clone();
+                    sf.Alignment = StringAlignment.Center;
+
+                    sf.LineAlignment = StringAlignment.Near;
+                    g.DrawString(sm[i], fn, Brushes.Green,
+                    new RectangleF(0, 0 + i * 5, pictureBox1.Size.Width - 20,
+                    pictureBox1.Size.Height - 20), sf);
+                    fn.Dispose();
+                }
+            }
+        }
+
+        private void InFile_Click_1(object sender, EventArgs e)
+        {
+            StreamWriter f = new StreamWriter(new FileStream(filename,
+            FileMode.Create, FileAccess.Write));
+
+            foreach (string s in sm) { f.WriteLine(s); }
+            f.Close();
+
+            MessageBox.Show("11 строк записано в файл!");
+        }
+    }
+}
